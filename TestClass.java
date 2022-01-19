587a74ac6f7f03358c9fd12fb54771fdf8d99373
Bharatwaaj Shankar <bharatwaaj.1996@gmail.com>
2022-01-19 17:22:51 -0400

    Minor changes test

--- TestClass.java
+++ TestClass.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class TestClass {
+public class TestClass {
+    public static void main(String args[]) {
+        System.out.println("Test Branch - changes!");
+    }
 }
