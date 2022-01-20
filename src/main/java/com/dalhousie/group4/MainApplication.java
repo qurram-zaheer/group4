2edc4f07a5f1fd59d4ee4cd702efd1cf662d07de
Bharatwaaj Shankar <bharatwaaj.1996@gmail.com>
2022-01-20 13:44:34 -0400

    Renamed Main File and Created a Few Packages

--- src/main/java/com/dalhousie/group4/MainApplication.java
+++ src/main/java/com/dalhousie/group4/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Group4Application {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Group4Application.class, args);
+		SpringApplication.run(com.dalhousie.group4.MainApplication.class, args);
 	}
 
 }
