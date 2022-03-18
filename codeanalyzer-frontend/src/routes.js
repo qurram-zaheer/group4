5ac7acb60ad10955f9daf7edaecf2c5e9a56522f
Bharatwaaj Shankar <bharatwaaj.1996@gmail.com>
2022-03-18 18:39:02 -0300

    Minor Changes

--- codeanalyzer-frontend/src/routes.js
+++ codeanalyzer-frontend/src/routes.js
@@ -23,6 +23,25 @@ import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
 
+
+var sideBarNav = [
+  {
+    path: "/index",
+    name: "Dashboard",
+    icon: "ni ni-tv-2 text-primary",
+    component: Index,
+    layout: "/admin",
+  },
+  {
+    path: "/repositories",
+    name: "Repositories",
+    icon: "ni ni-planet text-blue",
+    component: Icons,
+    layout: "/admin",
+  }
+]
+
+
 var routes = [
   {
     path: "/register",
