114bcba625d37aae2f1eefdcb3f2802c34e0b86f
Kavya <kv286760@dal.ca>
2022-03-20 23:16:38 -0300

    Registration changes

--- codeanalyzer-frontend/src/views/Index.js
+++ codeanalyzer-frontend/src/views/Index.js
@@ -38,6 +38,7 @@ import {
   Container,
   Row,
   Col,
+  Spinner,
 } from "reactstrap";
 
 // core components
@@ -77,6 +78,10 @@ const Index = (props) => {
         console.log('User Registration was Successful!');
       }
     }
+
+    //for fetching data from api 
+    api.fetchGithubRepo(userRegistration.data.user.username)
+    
   })()
   }, []);
 
