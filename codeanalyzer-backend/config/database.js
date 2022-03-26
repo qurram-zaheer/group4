59d4ff0c55048993f875a4e04a7c0843d6bcb899
Qurram Zaheer Syed <qr620423@dal.ca>
2022-03-26 13:53:26 -0300

    Migrating to postgres
    Took 55 minutes

--- codeanalyzer-backend/config/database.js
+++ codeanalyzer-backend/config/database.js
@@ -1,15 +1,35 @@
 const path = require('path');
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL)
 
-module.exports = ({ env }) => ({
+
+// module.exports = ({env}) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "localhost"),
+//       port: env("DATABASE_PORT", 3306),
+//       database: env("DATABASE_NAME", "ASDC_PROJECT"),
+//       user: env("DATABASE_USERNAME", "root"),
+//       password: env("DATABASE_PASSWORD", "password"),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+module.exports = ({env}) => ({
   connection: {
-    client: "mysql",
+    client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "ASDC_PROJECT"),
-      user: env("DATABASE_USERNAME", "root"),
-      password: env("DATABASE_PASSWORD", "password"),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
-    useNullAsDefault: true,
-  },
- });
+    debug: false
+  }
+})
