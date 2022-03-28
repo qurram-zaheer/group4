7132210ceac470f23c62257d3bbd3c418f8d8e4a
Qurram Zaheer Syed <qr620423@dal.ca>
2022-03-28 15:07:46 -0300

    rearranging files
    Took 22 minutes

--- codeanalyzer-backend/config/database.js
+++ codeanalyzer-backend/config/database.js
@@ -1,21 +1,5 @@
-const path = require('path');
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL)
-
-
-// module.exports = ({env}) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env("DATABASE_HOST", "localhost"),
-//       port: env("DATABASE_PORT", 3306),
-//       database: env("DATABASE_NAME", "ASDC_PROJECT"),
-//       user: env("DATABASE_USERNAME", "root"),
-//       password: env("DATABASE_PASSWORD", "password"),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
 
 module.exports = ({env}) => ({
   connection: {
--- codeanalyzer-frontend/src/routes.js
+++ codeanalyzer-frontend/src/routes.js
@@ -18,70 +18,69 @@
 import Dashboard from "views/Dashboard.js";
 import Profile from "views/examples/Profile.js";
 import Maps from "views/examples/Maps.js";
-import Register from "views/examples/Register.js";
-import Login from "views/examples/Login.js";
+import Register from "views/Register.js";
+import Login from "views/Login.js";
 import Tables from "views/examples/Tables.js";
-import Icons from "views/examples/Icons.js";
 import JiraAuth from "views/JiraAuth";
 
 var routes = [
-  {
-    path: "/register",
-    name: "Register",
-    icon: "ni ni-circle-08 text-pink",
-    component: Register,
-    layout: "/auth",
-  },
-  {
-    path: "/login",
-    name: "Login",
-    icon: "ni ni-key-25 text-info",
-    component: Login,
-    layout: "/auth",
-  },
-  {
-    path: "/index",
-    name: "Dashboard",
-    icon: "ni ni-tv-2 text-primary",
-    component: Dashboard,
-    layout: "/admin",
-  },
-  {
-    path: "/repositories",
-    name: "Repositories",
-    icon: "ni ni-collection text-blue",
-    component: JiraAuth,
-    layout: "/admin",
-    // sub_route: [
-    //   {
-    //     path: "/repositories/commits",
-    //     name: "Commits",
-    //     icon: "ni ni-bullet-list-67 text-blue",
-    //     component: JiraAuth,
-    //     layout: "/admin",
-    //   },
-    // ],
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "ni ni-pin-3 text-orange",
-    component: Maps,
-    layout: "/admin",
-  },
-  {
-    path: "/user-profile",
-    name: "User Profile",
-    icon: "ni ni-single-02 text-yellow",
-    component: Profile,
-    layout: "/admin",
-  },
-  {
-    path: "/tables",
-    name: "Tables",
-    icon: "ni ni-bullet-list-67 text-red",
-    component: Tables,
-    layout: "/admin",
-  },
+    {
+        path: "/register",
+        name: "Register",
+        icon: "ni ni-circle-08 text-pink",
+        component: Register,
+        layout: "/auth",
+    },
+    {
+        path: "/login",
+        name: "Login",
+        icon: "ni ni-key-25 text-info",
+        component: Login,
+        layout: "/auth",
+    },
+    {
+        path: "/index",
+        name: "Dashboard",
+        icon: "ni ni-tv-2 text-primary",
+        component: Dashboard,
+        layout: "/admin",
+    },
+    {
+        path: "/repositories",
+        name: "Repositories",
+        icon: "ni ni-collection text-blue",
+        component: JiraAuth,
+        layout: "/admin",
+        // sub_route: [
+        //   {
+        //     path: "/repositories/commits",
+        //     name: "Commits",
+        //     icon: "ni ni-bullet-list-67 text-blue",
+        //     component: JiraAuth,
+        //     layout: "/admin",
+        //   },
+        // ],
+    },
+    {
+        path: "/maps",
+        name: "Maps",
+        icon: "ni ni-pin-3 text-orange",
+        component: Maps,
+        layout: "/admin",
+    },
+    {
+        path: "/user-profile",
+        name: "User Profile",
+        icon: "ni ni-single-02 text-yellow",
+        component: Profile,
+        layout: "/admin",
+    },
+    {
+        path: "/tables",
+        name: "Tables",
+        icon: "ni ni-bullet-list-67 text-red",
+        component: Tables,
+        layout: "/admin",
+    },
 ];
 export default routes;
