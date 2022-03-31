138999911c52da836ccc28f93b868239ee647faf
Qurram Zaheer Syed <qr620423@dal.ca>
2022-03-31 16:21:46 -0300

    fixing git login bug
    Took 30 minutes

--- codeanalyzer-frontend/src/auth/PrivateRoute.js
+++ codeanalyzer-frontend/src/auth/PrivateRoute.js
@@ -1,11 +1,13 @@
-import { Route, Redirect } from 'react-router-dom'
-import { useUser } from './useUser';
+import {Redirect, Route} from 'react-router-dom'
+import {useUser} from './useUser';
+import useQuery from "../hooks/useQuery";
 
 export const PrivateRoute = props => {
+    const query = useQuery();
 
     const user = useUser();
 
-    if(!user) return <Redirect to="/login" />
+    if (!user && !query.get("access_token")) return <Redirect to="/login"/>
 
     return <Route {...props}/>
 }
