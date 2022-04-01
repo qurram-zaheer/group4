feacd46627de38a8cf16b931eca5a590668053d4
Bharatwaaj Shankar <bharatwaaj.1996@gmail.com>
2022-04-01 19:09:44 -0300

    User Story: Get Time Difference between Pull Request for a User

--- codeanalyzer-backend/src/api/pull-request/controllers/pull-request.js
+++ codeanalyzer-backend/src/api/pull-request/controllers/pull-request.js
@@ -6,4 +6,25 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::pull-request.pull-request');
+module.exports = createCoreController('api::pull-request.pull-request', ({ strapi }) => ({
+
+    // Get the time difference between pull requests of a user
+    async getAvgTimeDifferenceBetweenPullRequests(ctx, next) {
+        const differenceResult = [];
+        const prs = await strapi.db.query('api::pull-request.pull-request').findMany({
+            select:['id','createdOn'],
+            where: {
+                username: 'twendelmuth' //input TODO: parameterize this
+            },
+            orderBy: { createdOn: 'desc' }
+        });
+        for(let i = 0; i < prs.length - 1; i++){
+            let difference = (new Date(prs[i].createdOn).getTime() - new Date(prs[i+1].createdOn).getTime()) / (1000*60*60*24);
+            if(difference != 0){
+                differenceResult.push(difference);
+            }
+        }
+        ctx.body = differenceResult;
+    },
+
+}));
---
+++ codeanalyzer-backend/src/api/pull-request/routes/custom-pull-request-routes.js
@@ -0,0 +1,9 @@
+module.exports = {
+    routes: [
+      { 
+        method: 'GET',
+        path: '/pull-request/avgtimediff',
+        handler: 'pull-request.getAvgTimeDifferenceBetweenPullRequests',
+      }
+    ]
+}
