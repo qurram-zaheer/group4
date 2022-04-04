169a503bfd453ee718591c5dcd6c6b5ec46dd488
Qurram Zaheer Syed <qr620423@dal.ca>
2022-04-03 23:36:14 -0300

    adding count

--- codeanalyzer-backend/src/api/repository/controllers/repository.js
+++ codeanalyzer-backend/src/api/repository/controllers/repository.js
@@ -8,29 +8,18 @@ const { createCoreController } = require("@strapi/strapi").factories;
 const axios = require("axios");
 const Github = require("../../../app/github");
 
-module.exports = createCoreController('api::repository.repository', ({strapi}) => ({
-  async create(ctx) {
-    const {data} = ctx.request.body
-    const repoUrls = data.urls
-    const githubUser = data.user
-    const strapiUserId = githubUser.id
-    const repoData = await Github.getRepoDetailsBySlug({
-      repoSlugs: repoUrls,
-      accessToken: githubUser.accessToken
-    })
-
-    Promise.all(repoData.map(async r=>{
-      const repoEntry = {...r, user: githubUser.id, publishedAt: new Date().toISOString()}
-      console.log(repoEntry)
-      const entry = await strapi.entityService.create('api::repository.repository', {data: repoEntry})
-    }))
-    return {msg: 'Success'}
-  },
-
-  async getCounts(ctx) {
-    return strapi.query('api::repository.repository').count();
-  }
-}));
+module.exports = createCoreController(
+  "api::repository.repository",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const { data } = ctx.request.body;
+      const repoUrls = data.urls;
+      const githubUser = data.user;
+      const strapiUserId = githubUser.id;
+      const repoData = await Github.getRepoDetailsBySlug({
+        repoSlugs: repoUrls,
+        accessToken: githubUser.accessToken,
+      });
 
       Promise.all(
         repoData.map(async (r) => {
@@ -47,6 +36,10 @@ module.exports = createCoreController('api::repository.repository', ({strapi}) =
         })
       );
       return { msg: "Success" };
+    },
+
+    async getCounts(ctx) {
+      return strapi.query("api::repository.repository").count();
     },
   })
 );
