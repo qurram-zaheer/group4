9a18db4a6fca08ea9f69bd4393dccb85ef7b3840
Bharatwaaj Shankar <bharatwaaj.1996@gmail.com>
2022-04-03 23:35:28 -0300

    Repo Routes

--- codeanalyzer-frontend/src/repoRoutes.js
+++ codeanalyzer-frontend/src/repoRoutes.js
@@ -1,5 +1,6 @@
 import Repositories from "views/Repositories";
 import Commits from "./views/subviews/Commits";
+import PullRequests from "./views/subviews/PullRequests";
 
 const repoRoutes = [
   {
@@ -14,6 +15,13 @@ const repoRoutes = [
     name: "Commits",
     icon: "ni ni-circle-08 text-pink",
     component: Commits,
+    layout: "/admin",
+  },
+  {
+    path: "/repositories/pull-requests",
+    name: "Pull Requests",
+    icon: "ni ni-circle-08 text-pink",
+    component: PullRequests,
     layout: "/admin",
   },
 ];
