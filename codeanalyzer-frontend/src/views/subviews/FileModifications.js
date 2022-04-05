1f88044d15bb7f71618878ec04596b0088932194
Arshdeep Singh <ar899345@dal.ca>
2022-04-05 12:54:31 -0300

    Add skeleton of view FileModifications

---
+++ codeanalyzer-frontend/src/views/subviews/FileModifications.js
@@ -0,0 +1,79 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import { useState, useEffect } from "react";
+// react component that copies the given text inside your clipboard
+import { CopyToClipboard } from "react-copy-to-clipboard";
+// React Components
+import {
+  Badge,
+  Card,
+  CardHeader,
+  CardFooter,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Media,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Progress,
+  Table,
+  Container,
+  Row,
+  NavItem,
+  CardBody,
+  Nav,
+  NavLink,
+  Button,
+  Col,
+  Alert,
+  UncontrolledTooltip,
+} from "reactstrap";
+// core components
+import Header from "components/Headers/Header.js";
+// node.js library that concatenates classes (strings)
+import classnames from "classnames";
+// javascipt plugin for creating charts
+import Chart from "chart.js";
+// core components
+import { chartExample1, chartExample2, chartOptions, parseOptions, } from "variables/charts.js";
+
+import { api } from "../../lib/api"
+// react plugin used to create charts
+import { Bar, Line } from "react-chartjs-2";
+import axios from "axios";
+import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
+
+const FileModifications = () => {
+
+
+
+
+  const generateChartData = (data) => {
+  }
+
+  var data = {
+  };
+
+  return (
+     <></>
+  );
+};
+
+export default FileModifications;
